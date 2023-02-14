95c5e336cc65441ba1d77c034a575bdabd8b8dd0
finnickyfinn <finncastell04@gmail.com>
2023-02-14 14:52:28 -0500

    Java commit pt2

--- Akatsuki/assets/script.js
+++ Akatsuki/assets/script.js
@@ -1,12 +1,10 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-
-
-for(var x = 0; x < topics.length; x++) {
-  console.log(topics[x]);
+for (var x = 0; x < topics.length; x++) {
+  console.log(topics[x])
 }
 
-/*
 
+/*
 if (topic === 'HTML') {
   console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -18,8 +16,6 @@ if (topic === 'HTML') {
 } else {
   console.log('Please try again!');
 }
-var shapes = ["triangle", "square", "pentagon", "circle"];
-for(var x = 0; x < shapes.length; x++) {
- console.log(shapes[x]);
-}
+
 */
+
